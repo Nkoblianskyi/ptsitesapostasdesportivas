6e399c7c33ceb8966ae7588fb6004e77ba259c9c
Nykyta Koblianskyi <nkoblianskyi@gmail.com>
2025-12-11 20:28:22 +0100

    1

--- src/data/mock-data.tsx
+++ src/data/mock-data.tsx
@@ -17,7 +17,7 @@ export const bettingSites: BettingSite[] = [
     logo: "/blitz.svg",
     rating: 9.8,
     bonus: "200% até 10.000€",
-    welcomeOffer: "+ Aposta Grátis Onlywin 50% até {{1000€",
+    welcomeOffer: "+ Aposta Grátis Onlywin 50% até 1000€",
     votes: 3914,
     link: "https://qualityboost.top/dbk35YBJ",
   },
